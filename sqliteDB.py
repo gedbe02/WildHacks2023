64c16a444ee9ec0fc38d3c999a09cad48ae3d02e
alexsaavedraa <114758213+alexsaavedraa@users.noreply.github.com>
2023-04-15 21:32:08 -0500

    added initial db management thrupython sqlite3

---
+++ sqliteDB.py
@@ -0,0 +1,65 @@
+import sqlite3
+from sqlite3 import Error
+import os
+current_path = os.getcwd()
+dbname = "rocksdb.db"
+def create_connection(db_file):
+   
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+        
+
+        
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+    
+
+def delete_all_data():
+    conn = sqlite3.connect(dbname)
+
+    # Execute the query to delete all rows from a table
+    conn.execute('DELETE  FROM posts')
+
+    # Commit the changes to the database
+    conn.commit()
+
+def read_all_data():
+    conn = sqlite3.connect(dbname)
+    result = conn.execute('select * from posts')
+    for row in result:
+        print(row)
+
+
+
+
+def ingest_data(loc_x,loc_y,url):
+    conn = None
+
+    conn = sqlite3.connect(dbname)
+    print(sqlite3.version)
+    result = conn.execute('SELECT MAX(assetid) FROM posts').fetchone()
+    print(result[0])
+    if result[0] == None:
+        result = [0]
+
+    conn.execute(f"INSERT INTO posts (assetid, userid, location_x, location_y, photourl, artist, caption) VALUES ({int(result[0]+1)}, 10, {loc_x}, {loc_y}, '{url}', 'unknown', 'cool haha funny');")
+    res = conn.execute("select * from posts")
+    conn.commit()
+    for row in res:
+        print(row)
+    
+    
+
+#delete_all_data()
+#ingest_data(1,1,1)
+#read_all_data()
+
+
+
+
